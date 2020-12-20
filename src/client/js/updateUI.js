75ae1bd41a883e248d0e2a3e773f7e3cd9ede411
Thanos Kaloudis <38354865+Thanoskaloudis@users.noreply.github.com>
2020-12-20 22:56:04 +0200

    adding service workers and update css

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -21,19 +21,16 @@ function updateUI(days, img, dest, weather) {
             <div id="dest_weather">
                 <div id="card">
                     <div id="date">${weather.data[0].datetime}</div>
-                    <div id="icon"><img src="../media/${weather.data[0].weather.icon}.png" alt="${weather.data[0].weather.description}"></div>
                     <div id="temp">H:${weather.data[0].max_temp}° | L:${weather.data[0].min_temp}°</div>
                     <div id="pop">${weather.data[0].pop}%</div>
                 </div>
                 <div id="card">
                     <div id="date">${weather.data[1].datetime}</div>
-                    <div id="icon"><img src="../media/${weather.data[1].weather.icon}.png" alt="${weather.data[1].weather.description}"></div>
                     <div id="temp">H:${weather.data[1].max_temp}° | L:${weather.data[1].min_temp}°</div>
                     <div id="pop">${weather.data[1].pop}%</div>
                 </div>
                 <div id="card">
                     <div id="date">${weather.data[2].datetime}</div>
-                    <div id="icon"><img src="../media/${weather.data[2].weather.icon}.png" alt="${weather.data[2].weather.description}"></div>
                     <div id="temp">H:${weather.data[2].max_temp}° | L:${weather.data[2].min_temp}°</div>
                     <div id="pop">${weather.data[2].pop}%</div>
                 </div>
